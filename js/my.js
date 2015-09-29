eb274d2a541c24e6f5f570b0087a153c990f0b0c
Mayank <mayankjhawar18@gmail.com>
2015-09-30 00:16:31 +0530

    initial check in
    Code Check in

---
+++ js/my.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function() {
+    jQuery("img.lazy").lazy(
+        {
+            appendScroll    : window,
+            scrollDirection : "both",
+            effect: "fadeIn",
+            effectTime: 1000,
+            delay:500,
+            enableQueueing  : true
+        }
+    );
+});
+
+function imgError(image) {
+    image.onerror = "";
+    var hostName = window.location.host;
+    image.src = "http://localhost:9090/no-product.png";
+    return true;
+}
